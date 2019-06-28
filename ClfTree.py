d7a335162d2ba7290d3d90be321d44ec9a27528f
tianym1994 <tianym1994@163.com>
2019-06-28 14:20:04 +0800

    分类树和回归树

--- ClfTree.py
+++ ClfTree.py
@@ -3,13 +3,13 @@ from sklearn.model_selection import  train_test_split
 from  sklearn.metrics import  accuracy_score
 from sklearn.tree import  DecisionTreeClassifier
 from sklearn.datasets  import  load_iris
-from numpy  import emath
+
 #准备数据集
 iris=load_iris()
 #获取特征集和分类标识
 features=iris.data
 labels=iris.target
-#随机抽取33%的数据作为测试集，其他为训练集
+#随机抽取33%的数据作为测试集，其他为训练集这里的random_state就是为了保证程序每次运行都分割一样的训练集和测试集。否则，同样的算法模型在不同的训练集和测试集上的效果不一样。
 train_fratures,test_features,train_labels,test_labels=train_test_split(features,labels,test_size=0.33,random_state=0)
 #创建CART分类树sklearn
 clf=DecisionTreeClassifier(criterion='gini')
@@ -20,3 +20,4 @@ test_predict=clf.predict(test_features)
 #预测结果与测试集结果作对比
 score=accuracy_score(test_labels,test_predict)
 print('CART分类树准确率%.3lf'% score)
+
---
+++ DecTree.py
@@ -0,0 +1,25 @@
+#预测波士顿房价
+#encoding=utf-8
+from sklearn.model_selection import  train_test_split
+from sklearn.metrics import  mean_absolute_error
+from sklearn.datasets import  load_boston
+from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+from sklearn.tree import  DecisionTreeRegressor
+#准备数据集
+boston=load_boston()
+#探索数据
+print(boston.feature_names)
+#获取特征集和分类标识
+features=boston.data
+prices=boston.target
+#获取33%的数据作为测试集
+train_fratures,test_features,train_prices,test_prices=train_test_split(features,prices,test_size=0.33,random_state=0)
+#创建回归树
+dtr=DecisionTreeRegressor()
+#拟合构造CART回归树
+dtr.fit(train_fratures,train_prices)
+#预测测试集集中的房价
+predict_price=dtr.predict(test_features)
+#测试集的结果评价
+print('回归树二乘偏差均值：',mean_squared_error(test_prices,predict_price))
+print('回归树绝对值偏差均值：',mean_absolute_error(test_prices,predict_price))
